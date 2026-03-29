a7da9c1519f65d56ad447695f1c8a5ad0058e78b
Leonardo Lobo <leonardo.lobo@unet.edu.ve>
2026-03-29 13:46:43 -0400

    fix: modificate views and style

--- src/features/tableDashboard/type.ts
+++
@@ -1,9 +0,0 @@
-export interface DashboardRow {
-	id: string;
-	teammate: string;
-	repository: string;
-	status: "Pendiente" | "En revisión" | "Aprobado";
-	score: number;
-	updatedAt: string;
-	description: string;
-}
